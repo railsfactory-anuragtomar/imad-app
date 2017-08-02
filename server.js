301c0827d93ea1a33822acdf106f67cd0f9cdc4f
Anurag Tomar <anuragtomar@railsfactory.org>
2017-08-02 15:24:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/Article-one', function (req, res) {
+  res.send("Article one is served and will be requested");
+});
+
+app.get('/Article-two', function (req, res) {
+  res.send("Article two is served and will be requested");
+});
+
+app.get('/Article-three', function (req, res) {
+  res.send("Article three is served and will be requested");
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
